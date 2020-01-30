fbaa4c1a3930da779ec301368eb8ec8e5167b7ab
Sebastian Österberg <sebastian.osterberg@gmail.com>
2020-01-30 16:50:07 +0100

    Fix babel version/config

--- .babelrc.js
+++ .babelrc.js
@@ -2,5 +2,5 @@ module.exports = {
   presets: [['@babel/preset-env', {
     'targets': { 'node': 'current' }
   }], '@babel/preset-react'],
-  plugins: ['transform-es2015-spread']
+  plugins: ['@babel/plugin-transform-spread']
 }
