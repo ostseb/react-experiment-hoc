f50cc0ec5bac35caeb249467bd5a937b2850cdaa
Sebastian Österberg <sebastian.osterberg@gmail.com>
2020-01-31 09:10:52 +0100

    Switch to npm and fix build

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,3 @@
 module.exports = {
-  presets: [['@babel/preset-env', {
-    'targets': { 'node': 'current' }
-  }], '@babel/preset-react'],
-  plugins: ['@babel/plugin-transform-spread']
+  presets: ['@babel/preset-env', '@babel/preset-react'],
 }
